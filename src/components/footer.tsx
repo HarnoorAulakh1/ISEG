ce603d626829f8f76e2b113722384717c65a90e7
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2024-07-18 10:53:52 +0530

    search removed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,6 @@ function Footer() {
         Copyright © Indian Society of Engineering Geology, all rights reserved.
       </h1>
       <div className="w-full md:flex hidden  gap-5 items-center flex-row justify-center md:pl-10 md:pr-10 pl-5 pr-5">
-        <IoIosSearch className="cursor-pointer text-[1.2rem]" />
         <div className="flex flex-row gap-0">
           <p className="text-sm font-light border-x-[1px] cursor-pointer py-2 px-1 md:px-2 border-[#bdbcbc]">
             ISEG Golden Jublee
