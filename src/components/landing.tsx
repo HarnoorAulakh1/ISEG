4cbd9d077767a67b1471dee9113165fdfab710e4
saini128 <saini.hck@gmail.com>
2024-08-08 14:52:20 +0530

    landing edits

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -41,7 +41,7 @@ function Landing() {
       </div>
         <hr className="border-[1px]  bg-black border-black" />
       </div>
-      <div className="md:w-[85%] w-[95%] mt-5 text-[1rem] text-justify md:text-[1.5rem] bg-white m-auto border-[2px] border-black py-[2rem] md:px-[6rem] px-[1rem] flex flex-col gap-5">
+      <div className="md:w-[85%] w-[95%] mt-5 text-[1rem] text-justify md:text-[1.5rem] bg-white m-auto border-[2px] border-white py-[2rem] md:px-[6rem] px-[1rem] flex flex-col gap-5">
         <p>
           The Executive Council of the ISEG and Team EGCON 2025 take pride in
           inviting you to join them at the celebrations and to the conference.
